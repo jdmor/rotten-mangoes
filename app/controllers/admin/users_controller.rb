1133297948aec10b96e30fd5569948b60f11041c
jdmor <jona.morrissey@gmail.com>
2016-02-03 22:02:42 +0000

    Redirect /admin/users/new to /users/new and remove unneeded actions in admin/users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,38 +8,7 @@ class Admin::UsersController < ApplicationController
 
   def new
     @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    @user.admin = true
-
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to movies_path, notice: "Welcome aboard, #{@user.firstname}! Use your admin powers wisely!"
-    else
-      render :new
-    end
-  end
-
-  protected
-
-  def user_params
-    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :admin)
-  end
-
-  def restrict_access
-    if !current_user
-      flash[:alert] = 'You must log in.'
-      redirect_to new_session_path
-    elsif current_user && current_user.admin == false
-      flash[:alert] = 'This page is only accessible to admins.'
-      redirect_to new_session_path
-    end
-  end
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    redirect_to new_user_path
   end
 
 end
