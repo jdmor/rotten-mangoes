6d884d4a9a9ba62d0b7621ac776908c0ce5e9ac3
jdmor <jona.morrissey@gmail.com>
2016-02-03 05:26:59 +0000

    Generate admin/users namespaced controller and index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 RottenMangoes::Application.routes.draw do
 
-  get "reviews/new"
-  get "reviews/create"
   resources :movies do
     resources :reviews, only: [:new, :create]
   end
 
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+
   root to: 'movies#index'
+
+  namespace :admin do
+    resources :users
+  end
 
 end
