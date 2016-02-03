8f8ab8b3987be0bccf892762d12926e9cd9553c8
jdmor <jona.morrissey@gmail.com>
2016-02-03 07:13:57 +0000

    Add pagination to users table

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < ApplicationController
   before_filter :restrict_access
 
   def index
-    @users = User.all
+    @users = User.all.page(params[:page]).per(10)
   end
 
   def new
