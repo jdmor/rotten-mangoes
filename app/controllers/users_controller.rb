f118745beaaa2d1ef81842d8fbc230e7839a345d
jdmor <jona.morrissey@gmail.com>
2016-02-04 20:05:48 +0000

    Keep admin logged in when creating an account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,22 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      session[:user_id] = @user.id # auto log in
-      redirect_to movies_path, notice: "Welcome aboard, #{@user.firstname}!!"
+      set_user
+      new_user_notice
     else
       render :new
+    end
+  end
+
+  def set_user
+    session[:user_id] = @user.id if !current_user
+  end
+
+  def new_user_notice
+    if current_user && current_user.admin == true
+      redirect_to new_user_path, notice: "You've created an account for #{@user.firstname}."
+    else
+      redirect_to movies_path, notice: "Welcome aboard, #{@user.firstname}!!"
     end
   end
 
