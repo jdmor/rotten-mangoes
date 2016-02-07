6018917692c6ac733f78b2218d80a8571ce48b16
jdmor <jona.morrissey@gmail.com>
2016-02-07 09:41:29 +0000

    Create search box on home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+// require jquery
+// require jquery_ujs
+//= require jquery/jquery-2.2.0
 //= require materialize/bin/materialize
 //= require main-nav
+//= require form
 //= require turbolinks
