711af9ac04c39e6043b307f0fb170ee7c9a7ffea
jdmor <jona.morrissey@gmail.com>
2016-02-14 11:17:26 +0000

    Hide toggleable search box if large viewport

--- app/assets/javascripts/main-nav.js
+++ app/assets/javascripts/main-nav.js
@@ -5,4 +5,11 @@ $(function() {
     $(this).find('.is-transparent').toggleClass('is-transparent').siblings().toggleClass('is-transparent');
     $('#search-box').slideToggle();
   });
+
+  $(window).on('resize', function(e) {
+    if (window.innerWidth > 992) {
+      $('#search-box').hide();
+      $('#show-search .material-icons').removeClass('is-transparent').siblings().addClass('is-transparent');
+    }
+  });
 });
