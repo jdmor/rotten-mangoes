267798502fd3dd0d7f23e7fd90daf8c0b54f172d
jdmor <jona.morrissey@gmail.com>
2016-02-15 06:57:59 +0000

    Remove js initializer for main nav sidebar

--- app/assets/javascripts/main-nav.js
+++ app/assets/javascripts/main-nav.js
@@ -1,6 +1,4 @@
 $(function() {
-  $('.button-collapse').sideNav();
-
   $('#show-search').on('click', function() {
     $(this).find('.is-transparent').toggleClass('is-transparent').siblings().toggleClass('is-transparent');
     $('#search-box').slideToggle();
